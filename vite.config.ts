21df0d750180a8658d0693927e21f4bb98843554
Sam LaFell <lafell.samuel@gmail.com>
2025-01-20 18:54:49 -0500

    few small updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/react/", // Add this line - this is like setting your base_url in Python web frameworks
+});
